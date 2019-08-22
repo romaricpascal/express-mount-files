7d363c2de853d390ae10bbb7b7c9db55a41cdba5
Romaric Pascal <hello@romaricpascal.is>
2019-08-22 09:00:45 +0100

    feat: add base implementation of loading `routes.js` files

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,12 +2,11 @@ const test = require('ava');
 const express = require('express');
 const middleware = require('..');
 const request = require('supertest');
-const path = require('path');
 
 test.before(t => {
   const app = express();
   // Set up our middleware for testing
-  app.use(middleware(path.join(__dirname, 'routes')));
+  app.use(middleware('routes', { cwd: __dirname }));
   // Add a catch-all 404
   app.use((req, res) => res.sendStatus(404));
   // Prepare the supertest agent for querying
@@ -15,7 +14,7 @@ test.before(t => {
 });
 
 test('it loads a function in routes.js', t => {
-  return t.context.agent.get('/').then(response => {
-    t.assert(response.status == 404);
+  return t.context.agent.get('/routes-function').then(response => {
+    t.assert(response.status == 200);
   });
 });
--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-module.exports = function() {
-  return function(req, res, next) {
-    next();
-  };
+const path = require('path');
+const fastGlob = require('fast-glob');
+const { Router } = require('express');
+
+module.exports = function(root, { cwd = process.cwd() } = {}) {
+  root = path.resolve(cwd, root);
+
+  const router = new Router();
+
+  // First let's list the routes file
+  const routes = fastGlob.sync('**/routes.js', { cwd: root });
+  routes.forEach(route => {
+    // Grab the configuration, using the full path as we don't
+    // know where we are relative to this file
+    const fullPath = path.resolve(root, route);
+    const config = require(fullPath);
+
+    // Create a new router specific to this route
+    // configure it and set it up
+    const subRouter = new Router();
+    config(subRouter);
+    router.use('/' + path.dirname(route), subRouter);
+  });
+
+  return router;
 };
